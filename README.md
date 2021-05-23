bb4c649cd18adf853d56984e606a75057811dbc3
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-05-23 08:51:58 +0300

    cats-catcher

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # evo-ts-bootcamp
+
+8. [Cats catcher](https://nifty-nobel-522ba8.netlify.app/).
---
+++ homeworks/08-cats-catcher/src/App/index.tsx
@@ -0,0 +1,79 @@
+import { FC, useState, useEffect, useRef } from "react";
+import { of, interval, fromEvent, combineLatest } from "rxjs";
+import { map, filter } from "rxjs/operators";
+import random from "lodash/random";
+
+import { ReactComponent as WindowIcon } from "assets/icons/window.svg";
+import { ReactComponent as CatIcon } from "assets/icons/kitty.svg";
+import { Container, IconWrapper, ControllersContainer } from "./App.styled";
+import { useGenerateWindowsPositions } from "./generateFiled";
+
+const App: FC = () => {
+  const catRef = useRef<HTMLSpanElement>(null);
+  const resetButtonRef = useRef<HTMLButtonElement>(null);
+  const { positions, onReset } = useGenerateWindowsPositions();
+  const [catPosition, setCatPosition] = useState<null | number[]>(null);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const positions$ = of(positions);
+    const interval$ = interval(1000);
+
+    const cat$ = combineLatest([positions$, interval$])
+      .pipe(
+        filter(([pos]) => {
+          return !!pos;
+        }),
+        map(([pos]) => pos![random(0, 9)])
+      )
+      .subscribe((val) => setCatPosition(val));
+
+    const shoot$ = fromEvent(catRef.current!, "click").subscribe(() =>
+      setScore((prevState) => prevState + 1)
+    );
+
+    return () => {
+      cat$.unsubscribe();
+      shoot$.unsubscribe();
+    };
+  }, [positions]);
+
+  useEffect(() => {
+    const reset$ = fromEvent(resetButtonRef.current!, "click").subscribe(() => {
+      onReset();
+      setScore(0);
+      setCatPosition(null);
+    });
+
+    return () => {
+      reset$.unsubscribe();
+    };
+  }, [onReset]);
+
+  return (
+    <div>
+      <Container className="game-field">
+        {positions &&
+          positions.map(([x, y], index) => (
+            <IconWrapper key={index} x={x} y={y}>
+              <WindowIcon />
+            </IconWrapper>
+          ))}
+        <IconWrapper
+          className={catPosition ? "" : "hide"}
+          ref={catRef}
+          x={catPosition ? catPosition[0] : 0}
+          y={catPosition ? catPosition[1] : 0}
+        >
+          <CatIcon />
+        </IconWrapper>
+      </Container>
+      <ControllersContainer>
+        <p>Score: {score}</p>
+        <button ref={resetButtonRef}>Restart</button>
+      </ControllersContainer>
+    </div>
+  );
+};
+
+export default App;
