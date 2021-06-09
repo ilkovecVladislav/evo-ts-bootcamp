78d274cdd9e0346b2d03cab57b1a25f1c59fd124
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2021-06-09 14:40:42 +0300

    review fixes

--- homeworks/06-advanced-redux/src/utils.ts
+++ homeworks/06-advanced-redux/src/utils.ts
@@ -9,29 +9,26 @@ type LogData = {
   pizzaPrice?: number;
 };
 
-export const logger: Middleware<{}, RootState> = (store) => (next) => (
-  action
-) => {
-  if (action.type) {
-    const result: LogData = {
-      eventName: action.type,
-    };
-    if (typeof action.payload === "string") {
-      const pizza = store
-        .getState()
-        .pizzaList.pizzaList.find((item) => item._id === action.payload);
-      if (pizza) {
-        console.log(4);
-        result.pizzaName = pizza.name;
-        result.pizzaPrice = pizza.price;
+export const logger: Middleware<{}, RootState> =
+  (store) => (next) => (action) => {
+    if (action.type) {
+      const result: LogData = {
+        eventName: action.type,
+      };
+      if (typeof action.payload === "string") {
+        const pizza = store
+          .getState()
+          .pizzaList.pizzaList.find((item) => item._id === action.payload);
+        if (pizza) {
+          result.pizzaName = pizza.name;
+          result.pizzaPrice = pizza.price;
+        }
+      } else if (!Array.isArray(action.payload)) {
+        result.pizzaName = action.payload.name;
+        result.pizzaPrice = action.payload.price;
       }
-    } else if (!Array.isArray(action.payload)) {
-      result.pizzaName = action.payload.name;
-      result.pizzaPrice = action.payload.price;
+      logInfo(result);
     }
-    console.log(result);
-    logInfo(result);
-  }
 
-  return next(action);
-};
+    return next(action);
+  };
